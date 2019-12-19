d2b8d00e3b23bbb7556d0c45b5d1bae6eef13ef4
Kevin Tou <toukevin@gmail.com>
2019-12-19 12:11:42 -0800

    Added in_order_print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -213,12 +213,12 @@ class BinarySearchTree:
             if self.left == None:
                 self.left = BinarySearchTree(value)
             else:
-                return self.left.insert(value)
+                self.left.insert(value)
         elif value >= self.value:
             if self.right == None:
                 self.right = BinarySearchTree(value)
             else:
-                return self.right.insert(value)
+                self.right.insert(value)
 
         # Return True if the tree contains the value
         # False if it does not
@@ -250,17 +250,36 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
+        # Recursive
         if self.left is not None:
             self.left.for_each(cb)
         if self.right is not None:
             self.right.for_each(cb)
         cb(self.value)
 
+        # Iterative
+        # stack = Stack()
+        # stack.push(self)
+
+        # while stack.len() > 0:
+        #     current_node = stack.pop()
+        #     if current_node.right:
+        #         stack.push(current_node.right)
+        #     if current_node.left:
+        #         stack.push(current_node.left)
+        #     cb(current_node.value)
+
     # DAY 2 Project -----------------------
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        # Check left, if left is None, that's the smallest number
+        # Check right
+        if self.left is not None:
+            self.left.in_order_print(node)
+        print(self.value)
+        if self.right is not None:
+            self.right.in_order_print(node)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
